7774dca0247d015de8ea161072438f5e5b800942
Leader <leaderkhiva@gmail.com>
2025-11-03 21:36:51 +0500

    gitignore qo'shildi

---
+++ models/menu.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Foydalanuvchi: User
+ * Loyiha nomi: yangiliklar.uz
+ * Fayl nomi: menu.php
+ * Fayl yaratilgan: 03.11.2025 15:59
+ * Maqsad:
+ */
+
---
+++ models/socials.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Foydalanuvchi: User
+ * Loyiha nomi: yangiliklar.uz
+ * Fayl nomi: socials.php
+ * Fayl yaratilgan: 03.11.2025 16:58
+ * Maqsad:
+ */
+
