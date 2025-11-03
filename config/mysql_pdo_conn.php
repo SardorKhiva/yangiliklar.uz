a6fc5c71401400edd91423d5a73ae3f9a6145bb4
Leader <leaderkhiva@gmail.com>
2025-11-03 14:16:03 +0500

    controller jildi

--- config/mysql_pdo_conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once dirname(__DIR__) . '/config/CONSTANTS.php';
-
-try {
-    $pdo = new PDO(MYSQL_DSN, DB_USER, DB_PASSWORD);
-} catch (PDOException $e) {
-    echo "MySQL orqali ulanishda xatolik: " . $e->getMessage();
-}
--- controllers/mainController.php
+++ controllers/mainController.php
@@ -1,20 +1,3 @@
 <?php
-// mainController.php
 
-require_once __DIR__ . '/../config/CONSTANTS.php';
-require_once MAIN_MODEL;  // asosiy modelni qo'shish
-
-if (!empty($_GET) && !empty($_GET['controller'])) {
-    $controller = $_GET['controller'];
-        die($controller);
-//    switch ($controller) {
-//        case 'news':{
-//
-//        }
-//    }
-}
-
-// modeldagi ma'lumotlarni olish
-$getMenuItems = getMenuItems();
-$getSocailMediaItems = getSocialMediaItems();
-$news = getLastNews();
+require_once __DIR__ . '/../models/mainModel.php';
