0da05e7fa35840f0b3e03139e60ae5b14420afa7
Leader <leaderkhiva@gmail.com>
2025-11-03 21:39:35 +0500

    sqlite connect script

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+const HOST = '127.0.0.1';  // localhost
+const DB_NAME = 'mohirdev';
+const USER_NAME = 'root';
+const PASSWORD = '1302';
+const ACTIVE = 1;
+const SQLITE_DB_PATH = __DIR__. '/db/sqlite.sqlite3';  // SQLite baza yo'li 
+
+try {
+    $pdo = new PDO('sqlite:' . SQLITE_DB_PATH);
+//    echo 'SQLite bazaga ulandi!' . "<br>";
+} catch (PDOException $e) {
+    echo 'SQLite bazaga ulanishda xatolik bo\'ldi: ' . $e->getMessage() . "<br>";
+}
