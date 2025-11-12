2701a5492a704ab26153bd89ecb2a6e7e88a9631
Leader <leaderkhiva@gmail.com>
2025-11-12 22:39:21 +0500

    menu item delete helper js script added

---
+++ admin/assets/js/delete_menu_item.js
@@ -0,0 +1,14 @@
+ // Bu skript bazadan menu dagi qatorni o'chirishda yordam beradi
+ // alert('Hello!');
+ let deleteBtns = document.querySelectorAll(".delete_btn");
+deleteBtns.forEach(function (btn){
+    btn.addEventListener('click', function (e){
+        e.preventDefault();
+        let id = btn.getAttribute('data-id');
+        let confirmed = confirm("Rostdan ham siz menyu elementini o'chirmoqchimisiz?");
+
+        if (confirmed){
+            window.location.href = "?acontroller=menu_delete&id=" + id;
+        }
+    })
+})
